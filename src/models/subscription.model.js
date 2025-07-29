e47bc76bf5256d1f31715c5f283c63ee0c4d7113
pschauhan81 <ps8168372@gmail.com>
2025-07-29 13:34:50 +0530

    model formet solve error

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,15 +1,17 @@
-import mongoose , {Schema} from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({
-    subscriber:{
-        type: Schema.Types.ObjectId, // one who is subscribing
-        ref: 'User',
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
     },
     channel: {
-        type: Schema.Types.ObjectId, // the channel being subscribed to
-        ref: 'User',
-    }
-},{timestamps: true});
+      type: Schema.Types.ObjectId, // the channel being subscribed to
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-
-export const Subscription = mongoose.model('Subscription', subscriptionSchema);
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
