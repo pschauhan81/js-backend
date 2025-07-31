967b206af4e18b55ea9a803e430ed2c0cc571a8d
pschauhan81 <ps8168372@gmail.com>
2025-07-31 15:52:04 +0530

    feat: add getUserChannelProfile function to retrieve user channel profile with subscriber count

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -255,9 +255,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(new ApiResponse(200,
-       req.user,
-       "current user fetched sucessfully"));
+    .json(new ApiResponse(200, req.user, "current user fetched sucessfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -341,6 +339,38 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated sucessfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+  ]);
+});
+
 export {
   registerUser,
   loginUser,
@@ -350,4 +380,5 @@ export {
   getCurrentUser,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
